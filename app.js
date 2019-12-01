2d460b515f777b4ed75c0e8a403a44121883bb91
Raj Bhupendrakumar Prajapati <rpraja2@uic.edu>
2019-12-01 21:39:47 +0000

    directory address error fixed

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    navigator.serviceWorker.register('sw.js').then(function (reg) {
+
+        if (reg.installing) {
+            console.log('Service worker installing');
+        } else if (reg.waiting) {
+            console.log('Service worker installed');
+        } else if (reg.active) {
+            console.log('Service worker active');
+        }
+
+    }).catch(function (error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
     });
+}
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(function (registration) {
+            console.log('service worker registration succeeded:', registration);
+        },
+        function (error) {
+            console.log('service worker registration failed:', error);
+        });
+} else {
+    console.log('service workers are not supported.');
 }
